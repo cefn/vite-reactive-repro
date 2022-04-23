6a1bf25f285288efa276b1d92f8920eccae1dfcd
Cefn Hoile <cefn.hoile@snyk.io>
2022-04-23 12:30:17 +0100

    Inject lib-bundled app in minimal nginx page.

---
+++ src/extra/api/hook.ts
@@ -0,0 +1,6 @@
+import { reactive } from "vue";
+import { initialiseCacheContainer } from "./cache";
+
+export function useCache<T>(initialData?: T) {
+  return reactive(initialiseCacheContainer(initialData));
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    port: 3001,
+  },
+  mode: "development",
+  build: {
+    sourcemap: "inline",
+    lib: {
+      entry: "src/extra/mount/index.ts",
+      name: "Mount",
+      formats: ["iife"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      output: {
+        // manualChunks: undefined,
+        inlineDynamicImports: true,
+      },
+    },
+  },
+});
