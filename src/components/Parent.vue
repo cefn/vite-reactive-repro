74151c93fd227950922109fbba5c7c9f5a315e1f
Cefn Hoile <cefn.hoile@snyk.io>
2022-07-13 09:17:26 +0100

    Demonstrate a shared reactive

--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -1,14 +1,24 @@
 <template>
-  <Child />
+  <p>Parent toggle is {{ childState.toggle }}</p>
+  <Child :state="childState" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
+import { ChildState } from "../state";
 import Child from "./Child.vue";
 
 export default defineComponent({
   components: {
     Child,
+  },
+  setup() {
+    const childState = reactive<ChildState>({
+      toggle: false,
+    });
+    return {
+      childState,
+    };
   },
 });
 </script>
