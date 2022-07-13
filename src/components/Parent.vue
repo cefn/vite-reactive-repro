476cb83f58c7f59f1d7c1defcc024425d3153e46
Cefn Hoile <cefn.hoile@snyk.io>
2022-07-13 09:09:00 +0100

    Minimal rooted vue tree

---
+++ src/components/Parent.vue
@@ -0,0 +1,14 @@
+<template>
+  <Child />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Child from "./Child.vue";
+
+export default defineComponent({
+  components: {
+    Child,
+  },
+});
+</script>
